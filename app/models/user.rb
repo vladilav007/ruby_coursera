fea0ec27f91a07326176c0cfb568fe3a581ca01c
vladilav007 <vlad21452001@gmail.com>
2022-08-05 14:16:23 +0300

    devise confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, 
+         :confirmable
   
   has_many :courses
 
